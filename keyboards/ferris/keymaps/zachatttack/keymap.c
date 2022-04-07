71833c7aac3ec886a7b38bbbddaa925c4fdd5c7e
Zach Thomas <zach.thomas1.zt@gmail.com>
2022-04-07 13:22:09 -0500

    Added ctrl and shift homerow mods

--- keyboards/ferris/keymaps/zachatttack/keymap.c
+++ keyboards/ferris/keymaps/zachatttack/keymap.c
@@ -22,17 +22,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q, KC_W, KC_F, KC_P, KC_B, 
     KC_J, KC_L, KC_U, KC_Y, KC_SCLN,
 
-    KC_A, KC_R, KC_S, KC_T, KC_G, 
-    KC_M, KC_N, KC_E, KC_I, KC_O, 
+    KC_A, KC_R, MT(MOD_LSFT,KC_S), MT(MOD_LCTL,KC_T), KC_G, 
+    KC_M, MT(MOD_RCTL,KC_N), MT(MOD_RSFT,KC_E), KC_I, KC_O, 
 
     KC_Z, KC_X, KC_C, KC_D, KC_V, 
     KC_K, KC_H, KC_COMM,  KC_DOT, KC_SLSH, 
 
-    KC_P0, KC_BSPC, 
-    LT(7,KC_SPC), KC_P1
+    KC_LEFT_SHIFT, KC_BSPC, 
+    LT(1,KC_SPC), KC_NO
   ),
 
-	[7] = LAYOUT_split_3x5_2(
+	[1] = LAYOUT_split_3x5_2(
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
 
