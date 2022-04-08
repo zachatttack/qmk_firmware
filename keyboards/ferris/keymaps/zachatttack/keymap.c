8f7f6791408f7099329d3597ed0260edadafd926
Zach Thomas <zach.thomas1.zt@gmail.com>
2022-04-08 12:45:45 -0500

    add more keys

--- keyboards/ferris/keymaps/zachatttack/keymap.c
+++ keyboards/ferris/keymaps/zachatttack/keymap.c
@@ -22,18 +22,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q, KC_W, KC_F, KC_P, KC_B, 
     KC_J, KC_L, KC_U, KC_Y, KC_SCLN,
 
-    KC_A, KC_R, MT(MOD_LSFT,KC_S), MT(MOD_LCTL,KC_T), KC_G, 
-    KC_M, MT(MOD_RCTL,KC_N), MT(MOD_RSFT,KC_E), KC_I, KC_O, 
+    KC_A, KC_R, MT(MOD_LSFT,KC_S), MT(MOD_LCTL,KC_T), MT(MOD_LGUI,KC_G), 
+    MT(MOD_RALT,KC_M), MT(MOD_RCTL,KC_N), MT(MOD_RSFT,KC_E), KC_I, KC_O, 
 
     KC_Z, KC_X, KC_C, KC_D, KC_V, 
     KC_K, KC_H, KC_COMM,  KC_DOT, KC_SLSH, 
 
-    LT(2,KC_DELETE), KC_BSPC, 
+    LT(2,KC_DELETE), LT(4,KC_BSPC), 
     LT(1,KC_SPC), LT(3,KC_NO)
   ),
   
-	[1] = LAYOUT_split_3x5_2(
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
+	[1] = LAYOUT_split_3x5_2( //Outside keys
+    KC_TAB, KC_NO, KC_NO, KC_NO, KC_NO, 
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
 
     KC_ESC, KC_NO, KC_NO, KC_NO, KC_NO, 
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_TRNS
   ),
 
-	[2] = LAYOUT_split_3x5_2(
+	[2] = LAYOUT_split_3x5_2( //Numbers
     KC_1, KC_2, KC_3, KC_4, KC_5, 
     KC_6, KC_7, KC_8, KC_9, KC_0, 
 
@@ -60,15 +60,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_0
   ),
 
-	[3] = LAYOUT_split_3x5_2(
-    KC_TILD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-    KC_DOWN, KC_RIGHT, KC_TRNS, KC_TRNS, KC_TRNS, 
+	[3] = LAYOUT_split_3x5_2( //Symbols
+    KC_TILD, KC_UNDS, KC_TRNS, KC_TRNS, KC_TRNS, 
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
 
     KC_DLR, KC_LBRC, KC_LPRN, KC_LCBR, KC_AMPR, 
     KC_ASTR, KC_RCBR, KC_RPRN, KC_RBRC, KC_CIRC, 
 
     KC_EXLM, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-    KC_UP, KC_LEFT, KC_TRNS, KC_TRNS, KC_TRNS, 
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+
+    KC_TRNS, KC_TRNS, 
+    KC_TRNS, KC_TRNS
+  ),
+
+	[4] = LAYOUT_split_3x5_2( //Nav
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+    KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_TRNS, 
+
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
 
     KC_TRNS, KC_TRNS, 
     KC_TRNS, KC_TRNS
