2a3f7efddf8563000a152a6ff7c8528a2edd7bc8
Zach Thomas <zach.thomas1.zt@gmail.com>
2022-06-14 15:42:42 -0500

    Add caps word to ferris

--- keyboards/ferris/keymaps/zachatttack/config.h
+++ keyboards/ferris/keymaps/zachatttack/config.h
@@ -45,3 +45,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   #define RGBLIGHT_SAT_STEP 8
   #define RGBLIGHT_VAL_STEP 8
 #endif
+
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
