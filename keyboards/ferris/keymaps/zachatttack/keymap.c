b1566f790ef7c2a83015b1ccdaf568f285c81ed8
Zach Thomas <zach.thomas1.zt@gmail.com>
2022-08-25 07:41:51 -0500

    Add grave and gui to lalt

--- keyboards/ferris/keymaps/zachatttack/keymap.c
+++ keyboards/ferris/keymaps/zachatttack/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q, KC_W, KC_F, KC_P, KC_B, 
     KC_J, KC_L, KC_U, KC_Y, KC_SCLN,
 
-    KC_A, KC_R, MT(MOD_LSFT,KC_S), MT(MOD_LCTL,KC_T), MT(MOD_LGUI,KC_G), 
+    KC_A, KC_R, MT(MOD_LSFT,KC_S), MT(MOD_LCTL,KC_T), MT(MOD_LALT,KC_G), 
     MT(MOD_RALT,KC_M), MT(MOD_RCTL,KC_N), MT(MOD_RSFT,KC_E), KC_I, KC_O, 
 
     KC_Z, KC_X, KC_C, KC_D, KC_V, 
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
 	[4] = LAYOUT_split_3x5_2( //Nav
-    KC_TAB, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+    KC_GRV, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
     KC_TRNS, KC_TRNS, KC_UP ,KC_TRNS, KC_TRNS, 
 
     KC_CAPS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
